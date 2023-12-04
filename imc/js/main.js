d2261087d8f479a4c57b770bb9ea07cfc0c6fb16
Kleiton Serra Pachêco <ART0062356@aluno.firjansenaisesi.com.br>
2023-12-04 20:04:10 -0300

    const

---
+++ imc/js/main.js
@@ -0,0 +1,6 @@
+const limparCampos = () => {
+    const nome = document.getElementById("nome").value = null;
+    const altura = document.getElementById("altura").value = null;
+    const peso = document.getElementById("peso").value = null;
+    
+}
